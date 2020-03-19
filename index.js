7117304cfeb34fc5de98190dfdda6287744a8e86
Juanse Calviño <j.s.calvinio@gmail.com>
2020-03-19 13:02:07 -0300

    Create start script

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+// Indico la url del frontend para permitirle el cruce de datos
+const corsOptions = { origin: "http://localhost:3000" };
+app.use(cors(corsOptions));
+
+// Middleware body parser
 app.use(express.json());
 
 app.listen('5000', function(){
